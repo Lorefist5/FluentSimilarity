66490cb503b0571f8dc2c5326a5d9670b23eb053
00arc7 <00arc7@D-DS-SF-13189>
2024-09-10 14:43:31 -0400

    When coditions added

--- FluentSimilarity/Builder/RuleBuilder.Main.cs
+++ FluentSimilarity/Builder/RuleBuilder.Main.cs
@@ -1,7 +1,4 @@
-﻿using SimMetrics.Net.Metric;
-using XSoundex;
-
-namespace FluentSimilarity.Builder;
+﻿namespace FluentSimilarity.Builder;
 
 public partial class RuleBuilder<T, TProperty>
 {
@@ -9,6 +6,7 @@ public partial class RuleBuilder<T, TProperty>
     private readonly string _propertyName;
     private readonly List<Func<T, T, double>> _comparisons = new();
     private readonly List<Func<double, bool>> _filters = new();
+    private Func<TProperty, bool> _propertyCondition = _ => true;
 
     public RuleBuilder(Func<T, TProperty> propertySelector, string propertyName)
     {
@@ -33,14 +31,22 @@ public partial class RuleBuilder<T, TProperty>
         _filters.Add(score => score >= amount);
         return this;
     }
-    public RuleBuilder<T, TProperty> NotIn(string[] values)
-    {
-        _filters.Add(score => !values.Contains(score.ToString()));
-        return this;
-    }
+
     public RuleBuilder<T, TProperty> NotHigherThan(double amount)
     {
         _filters.Add(score => score <= amount);
+        return this;
+    }
+
+    public RuleBuilder<T, TProperty> When(Func<TProperty, bool> condition)
+    {
+        _propertyCondition = condition;
+        return this;
+    }
+
+    public RuleBuilder<T, TProperty> WhenNot(Func<TProperty, bool> condition)
+    {
+        _propertyCondition = prop => !condition(prop);
         return this;
     }
 
@@ -48,6 +54,14 @@ public partial class RuleBuilder<T, TProperty>
     {
         return (obj1, obj2) =>
         {
+            var prop1 = _propertySelector(obj1);
+            var prop2 = _propertySelector(obj2);
+
+            if (!_propertyCondition(prop1) || !_propertyCondition(prop2))
+            {
+                return -1.0;
+            }
+
             var scores = _comparisons.Select(comp => comp(obj1, obj2));
             var filteredScores = scores.Where(score => _filters.All(filter => filter(score)));
             return filteredScores.Any() ? filteredScores.Max() : -1.0;
